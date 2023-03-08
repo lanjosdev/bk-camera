1ddb12b1cc86f66b040df39f805e49e28199bc0f
Renato Caetano <renato.caetano@bizsys.com.br>
2023-03-08 10:32:31 -0300

    analise de amoçoes back end

---
+++ back_end/emotionRecognition.py
@@ -0,0 +1,110 @@
+# Importing required packages
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+from keras.models import load_model
+import numpy as np
+import dlib
+import cv2
+import sys
+import json
+import npencoder
+
+list_of_arguments = sys.argv
+
+emotion_offsets = (20, 40)
+emotions = {
+    0: {
+        "emotion": "Angry",
+        "color": (193, 69, 42)
+    },
+    1: {
+        "emotion": "Disgust",
+        "color": (164, 175, 49)
+    },
+    2: {
+        "emotion": "Fear",
+        "color": (40, 52, 155)
+    },
+    3: {
+        "emotion": "Happy",
+        "color": (23, 164, 28)
+    },
+    4: {
+        "emotion": "Sad",
+        "color": (164, 93, 23)
+    },
+    5: {
+        "emotion": "Suprise",
+        "color": (218, 229, 97)
+    },
+    6: {
+        "emotion": "Neutral",
+        "color": (108, 72, 200)
+    }
+}
+
+
+def shapePoints(shape):
+    coords = np.zeros((68, 2), dtype="int")
+    for i in range(0, 68):
+        coords[i] = (shape.part(i).x, shape.part(i).y)
+    return coords
+
+
+def rectPoints(rect):
+    x = rect.left()
+    y = rect.top()
+    w = rect.right() - x
+    h = rect.bottom() - y
+    return (x, y, w, h)
+
+
+faceLandmarks = "faceDetection/models/dlib/shape_predictor_68_face_landmarks.dat"
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(faceLandmarks)
+
+emotionModelPath = 'models/emotionModel.hdf5'  # fer2013_mini_XCEPTION.110-0.65
+emotionClassifier = load_model(emotionModelPath, compile=False)
+emotionTargetSize = emotionClassifier.input_shape[1:3]
+
+filename = list_of_arguments[1]
+emotion_probability = 0
+emotion_label_arg = 6
+
+while True:
+    frame = cv2.imread('images/'+filename)
+    frame = cv2.resize(frame, (1440, 960))
+
+    grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    rects = detector(grayFrame, 0)
+    for rect in rects:
+        shape = predictor(grayFrame, rect)
+        points = shapePoints(shape)
+        (x, y, w, h) = rectPoints(rect)
+        grayFace = grayFrame[y:y + h, x:x + w]
+        try:
+            grayFace = cv2.resize(grayFace, emotionTargetSize)
+        except:
+            break
+
+        grayFace = grayFace.astype('float32')
+        grayFace = grayFace / 255.0
+        grayFace = (grayFace - 0.5) * 2.0
+        grayFace = np.expand_dims(grayFace, 0)
+        grayFace = np.expand_dims(grayFace, -1)
+
+        emotion_prediction = emotionClassifier.predict(grayFace)
+        emotion_probability = np.max(emotion_prediction)
+
+        emotion_label_arg = np.argmax(emotion_prediction)
+        if emotion_label_arg == 0:
+            emotion_label_arg = 4
+    break
+
+obj = {
+    "emotion_probaility": emotion_probability,
+    "emotion_type": emotion_label_arg,
+    "emotion_name": emotions[emotion_label_arg]["emotion"],
+}
+json_str = json.dumps(obj, cls=npencoder.NpEncoder)
+print(json_str)
